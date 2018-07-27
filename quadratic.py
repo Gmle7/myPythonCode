094c4818b47c305f3f17cf2cf79fe5671643d1d9
170228 <chenchao@chalco-steering.com>
2018-07-27 16:01:50 +0800

    格式化代码

--- quadratic.py
+++ quadratic.py
@@ -1,12 +1,10 @@
 import math
 
-
 def quadratic(a, b, c):
     drt = b * b - 4 * a * c
     if drt >= 0:
         return (-b + math.sqrt(drt)) / (2 * a), (-b - math.sqrt(drt)) / (2 * a)
     else:
         return 0
-
 
 print(quadratic(2, 7, 5))
--- testFunction.py
+++ testFunction.py
@@ -3,12 +3,10 @@ i = int(input('请输入一个整数'))
 print('十进制%s转为十六进制数为%s' % (i, hex(i)))
 print(hex(65536))
 
-
 def c_abs(x):
     if x >= 0:
         return x
     else:
         return -x
-
 
 print(c_abs(111))
